d1ff63347d3ba6c64aa109009a5da211ca1ba622
AydanoReis <120851157+AydanoReis@users.noreply.github.com>
2026-05-13 23:28:36 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,28 @@
-self.addEventListener('install', e => e.waitUntil(
-  caches.open('folgas-32bpm-v1').then(cache =>
-    cache.addAll(['/', '/index.html'])
-  )
-));
+const CACHE_VERSION = 'folgas-32bpm-v13052026';
 
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_VERSION).then(cache =>
+      cache.addAll(['/', '/index.html'])
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_VERSION)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
